0dc1e4789d6102eea0ffd75991bfd8472121a748
Mihai Pasnicu <mihai@Mihais-MacBook-Pro.local>
2018-09-25 20:20:09 -0400

    problem 2

---
+++ homework2/broyden.m
@@ -0,0 +1,18 @@
+function p_sol = broyden(maxit, tol, f, p, iJac)
+
+fVal = f(p);
+for iter = 1:maxit
+    fprintf('iter %d: p(1) = %f, p(2) = %f, norm(f(x)) = %.8f\n', iter, p(1), p(2), norm(fVal));
+    if norm(fVal) < tol
+        break
+    end
+    d = - (iJac * fVal);
+    p = p+d;
+    fOld = fVal;
+    fVal = f(p);
+    u = iJac*(fVal - fOld);
+    iJac = iJac + ( (d - u) * (d'*iJac) )/ (d'*u);
+end
+
+p_sol = p;
+end
--- homework2/homework2.m
+++ homework2/homework2.m
@@ -12,4 +12,14 @@ fprintf('Demand for A is %.2f and Demand for B is %.2f\n', demand(v_a, p_a, v_b,
 
 
 %% problem 2
-%
+% keep v_a = v_b = 2
+
+p = [1 2];
+f = @(x) [x(1) - 1/( 1 - demand(v_a, x(1), v_b, x(2))); x(2) - 1/(1 - demand(v_b, x(2), v_a, x(1)))];
+iJac = inv(myJac(f, p));
+
+maxit = 100; 
+tol = 1e-10; 
+tic
+p_sol = broyden(maxit, tol, f, p, iJac);
+toc

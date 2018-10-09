f84a6fae15699092348659df9534bbd7a3ac252b
Mihai Pasnicu <mihai@Mihais-MacBook-Pro.local>
2018-10-09 16:45:11 -0400

    p2

--- homework3/hw3_code.m
+++ homework3/hw3_code.m
@@ -4,7 +4,7 @@ y = M.y;
 
 %% problem 1
 
-b1 = [3 0 0 0 0 0];
+b1 = [1 0 0 0 0 0];
 f =@(b) - log_lik(x ,y ,b);
 
 maxit = 100;
@@ -17,3 +17,17 @@ for i = 1: maxit
     end
 end
 toc
+b1
+
+%% problem 2
+b2 = [1 0 0 0 0 0];
+f2 = @(b) norm(log_lik(x, y, b));
+tic
+b2 = fminunc(f2, b2, optimset('Display','final'));
+toc
+b2
+
+
+%% problem 3
+
+

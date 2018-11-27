ca0f3962deb61756fd4439d8ba82eeee475c7f3f
Mihai Pasnicu <mihai@Mihais-MacBook-Pro.local>
2018-11-27 00:51:59 -0500

    prob 2

---
+++ homework5/gauss.m
@@ -0,0 +1,9 @@
+function l = gauss(gamma, beta, sigma, Y, X, Z)
+
+optset('qnwnorm','usesqrtm',1);
+
+[x, w] = qnwnorm(100, beta, sigma);% Generate nodes
+nodes.nx = x;
+nodes.ny = 0;
+l = -like(gamma, w, nodes, Y, X, Z);
+end

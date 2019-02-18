2a1373d1184180b133d7101cc08997307a40dbc7
Mihai Pasnicu <mihai@Mihais-MacBook-Pro.local>
2019-02-18 01:26:57 -0500

    corrected version

---
+++ homework7/parameters.m
@@ -0,0 +1,28 @@
+% just a file with the parameters
+
+global L c beta t_0 t_1 delta lambda error eta maxiter v; 
+
+
+L = 30;
+ro = 0.85;
+k = 10;
+l = 15;
+v = 10;
+delta = 0.03;
+beta = 1 / 1.05;
+lambda = 1;
+eta = log(ro) / log(2);
+maxiter = 1000;
+error = 10e-5;
+
+% cost function
+c = zeros(L, 1);
+c(1: l - 1) = k .* (1 : 1 : l - 1) .^ eta;
+c(l: L) = k * l ^ eta;
+
+
+
+% transition probability for q = 0, 1 respectively
+
+t_0 = diag([1 (1 - delta) .^ (2 : 1 : L)], 0) + diag(1 - (1 - delta) .^ (2 : 1 : L), - 1) ;
+t_1 = diag([1 - (1 - delta) .^ (1 : 1 : L - 1) 1], 0) + diag((1 - delta) .^ (1 : 1 : L - 1), 1);
